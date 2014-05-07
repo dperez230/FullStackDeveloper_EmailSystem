6756e01a1ff6bc8282a9d3a6147994ef4c6f80c0
David Lindemann <accession.ryu@gmail.com>
2014-05-06 21:41:18 -0400

    Added line to display how git works

--- FullStackDeveloper_EmailSystem/src/HelloWorld.java
+++ FullStackDeveloper_EmailSystem/src/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Email System");
+		System.out.println("Goodbye Email System.");
 
 	}
 
