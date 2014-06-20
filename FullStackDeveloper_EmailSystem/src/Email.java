d04bd32553e6be20245e42676ca6a5a503c8669d
Damaris Perez <magicmirror11@aol.com>
2014-06-19 22:31:05 -0400

    -a

---
+++ FullStackDeveloper_EmailSystem/src/Email.java
@@ -0,0 +1,14 @@
+
+class Email {
+
+	private String to;
+	private String from;
+	private String subject;
+	private String date;
+	private int numOfDays;
+	private String body;
+	private String[] attachmentNames;
+	private boolean attachment;
+	private boolean read;
+	
+}
